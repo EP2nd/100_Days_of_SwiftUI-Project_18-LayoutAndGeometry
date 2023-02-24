4d5eb731a1de5c18fef19530030a8dda86214d0e
Edwin Przeźwiecki Jr <edwin.przezwiecki.jr@icloud.com>
2023-02-24 02:55:18 +0100

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Edwin Przeźwiecki Jr. on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
