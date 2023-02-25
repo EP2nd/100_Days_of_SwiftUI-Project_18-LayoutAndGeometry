8f178e26e5e0d021bf104caf551e0b4455da8492
Edwin Przeźwiecki Jr <edwin.przezwiecki.jr@icloud.com>
2023-02-25 14:27:34 +0100

    Solutions to all three challenges

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+//    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                        /// Challenge 3:
+//                            .background(colors[index % 7])
+                            .background(Color(hue: min(1.0, geo.frame(in: .global).minY / fullView.size.height), saturation: 1, brightness: 1))
+                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                        /// Challenge 1:
+                            .opacity(geo.frame(in: .global).minY / 200)
+                        /// Challenge 2:
+                            .scaleEffect(max(0.5, geo.frame(in: .global).minY / 500))
+                    }
+                    .frame(height: 40)
+                }
+            }
         }
-        .padding()
     }
 }
 
